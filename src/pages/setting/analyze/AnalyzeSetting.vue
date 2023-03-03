2520fdf674c597e002924baa59534db12d356873
Hiram <>
2023-03-03 18:25:21 +0800

    fix table error

--- src/pages/setting/analyze/AnalyzeSetting.vue
+++ src/pages/setting/analyze/AnalyzeSetting.vue
@@ -34,12 +34,12 @@
       @select-change="rehandleSelectChange"
       @page-change="rehandlePageChange"
     >
-      <!-- <template #name="{ row }">
+      <template #name="{ row }">
         <t-badge v-if="row.id === defaultAnalyze" size="small" :offset="[-5, 0]" count="默认" dot>{{
           row.name
         }}</t-badge>
         <span v-else>{{ row.name }}</span>
-      </template> -->
+      </template>
       <template #isActive="{ row }">
         <t-switch v-model="row.isActive" @change="propChangeEvent(row)">
           <template #label="tip">{{ tip.value ? '开' : '关' }}</template>
