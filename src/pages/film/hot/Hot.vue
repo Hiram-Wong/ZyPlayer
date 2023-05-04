9701e5dad0396e3c498183bfc1bab4518a89eb97
Hiram <wy-hai@qq.com>
2023-05-04 22:58:41 +0800

    fix hot

--- src/pages/film/hot/Hot.vue
+++ src/pages/film/hot/Hot.vue
@@ -1,37 +1,69 @@
 <template>
-  <t-dialog v-model:visible="formVisible" header="热榜" placement="center" :footer="false">
+  <t-dialog v-model:visible="formVisible" header="热搜榜" placement="center" :footer="false">
     <template #body>
-      <div class="hot-container">
+      <div class="hot-container dialog-container-padding">
         <div class="rax-view-v2 tab-container">
           <div class="rax-view-v2 tab-wrap">
-            <div v-for="item in MODE_OPTIONS" :key="item.key" class="rax-view-v2 tab-item">
-              {{ item.name }}
+            <div class="tab-item">
+              <span class="title">分类</span>
+              <t-radio-group v-model="hotClass" variant="default-filled" size="small" @change="changeHotClass">
+                <t-radio-button v-for="(item, key, index) in MODE_OPTIONS" :key="index" :value="key">
+                  {{ item.name }}
+                </t-radio-button>
+              </t-radio-group>
+            </div>
+            <div class="tab-item">
+              <span class="title">来源</span>
+              <t-radio-group v-model="hotSource" variant="default-filled" size="small" @change="changeHotSource">
+                <t-radio-button v-for="item in MODE_OPTIONS[hotClass].source" :key="item.key" :value="item.key">
+                  {{ item.name }}
+                </t-radio-button>
+              </t-radio-group>
             </div>
           </div>
-          <t-radio-group v-model="hotSource" variant="default-filled" size="small">
-            <t-radio-button v-for="(item, key) in MODE_OPTIONS[hotClass - 1].source" :key="key" :value="key">
-              {{ item }}{{ hotClass }}
-            </t-radio-button>
-          </t-radio-group>
         </div>
-        <div class="swiper swiper-initialized swiper-horizontal swiper-ios">
-          <div class="swiper-wrapper">
-            <div v-for="(item, index) in hotList" :key="item.vod_id" class="rax-view-v2 news-item">
-              <div class="rax-view-v2 news-rank rank-1">{{ index + 1 }}</div>
-              <div class="rax-view-v2 normal-view">
-                <div class="rax-view-v2 normal-title">{{ item.vod_name }}</div>
-                <div class="rax-view-v2 normal-tip-icon">{{ item.vod_hot }}</div>
+        <div class="swiper">
+          <t-skeleton theme="paragraph" :loading="loading">
+            <div class="swiper-wrapper">
+              <div
+                v-for="(item, index) in hotList"
+                :key="item.vod_id"
+                class="rax-view-v2 news-item"
+                @click="searchEvent(item)"
+              >
+                <div class="rax-view-v2 news-rank" :class="[index in [0, 1, 2] ? `rank-${index + 1}` : '']">
+                  {{ index + 1 }}
+                </div>
+                <div class="rax-view-v2 normal-view">
+                  <div class="rax-view-v2 normal-title">{{ item.vod_name }}</div>
+                  <div class="rax-view-v2 normal-tip" :class="[index in [0, 1, 2] ? `color-${index + 1}` : '']">
+                    {{ item.vod_hot }}
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
+          </t-skeleton>
         </div>
-        <p class="tip bottom-tip">数据来源: 酷云EVE</p>
+        <div class="tip-warp">
+          <span>数据来源：</span>
+          <t-link
+            theme="primary"
+            underline
+            href="http://eye.kuyun.com/pages/whole-network/whole-network"
+            target="_blank"
+          >
+            酷云EVE
+          </t-link>
+        </div>
       </div>
     </template>
   </t-dialog>
 </template>
 
 <script setup lang="ts">
+import { useIpcRenderer } from '@vueuse/electron';
+import moment from 'moment';
+import { MessagePlugin } from 'tdesign-vue-next';
 import { reactive, ref, watch } from 'vue';
 
 import zy from '@/lib/utils/tools';
@@ -50,14 +82,18 @@ const props = defineProps({
   },
 });
 
-const hotClass = ref(2);
+const store = usePlayStore();
+const ipcRenderer = useIpcRenderer();
+
+const hotClass = ref('movie');
 const hotSource = ref(1);
 
 const loading = ref(true); // 骨架屏是否显示热播
 
 const formVisible = ref(false); // dialog是否显示热播
 const hotList = ref([]); // 热播列表
-
+const formData = ref(props.site); // 接受父组件参数
+const formDetailData = ref({}); // 详情组件传参
 const emit = defineEmits(['update:visible']);
 
 watch(
@@ -73,25 +109,98 @@ watch(
     if (val) getHotList();
   },
 );
+watch(
+  () => props.site,
+  (val) => {
+    formData.value = val;
+  },
+);
 
-const MODE_OPTIONS = reactive([
-  {
+const MODE_OPTIONS = reactive({
+  movie: {
     key: 1,
     name: '电影',
-    source: { 1: '腾讯视频', 2: '爱奇艺', 3: '优酷', 4: '芒果' },
+    source: [
+      { key: 1, name: '腾讯视频' },
+      { key: 2, name: '爱奇艺' },
+      { key: 3, name: '优酷' },
+    ],
   },
-  { key: 2, name: '剧集', source: { 1: '腾讯视频', 2: '爱奇艺', 3: '优酷', 4: '芒果' } },
-  {
+  episode: {
+    key: 2,
+    name: '剧集',
+    source: [
+      { key: 1, name: '腾讯视频' },
+      { key: 2, name: '爱奇艺' },
+      { key: 3, name: '优酷' },
+      { key: 4, name: '芒果' },
+    ],
+  },
+  variety: {
     key: 3,
     name: '综艺',
-    source: { 1: '腾讯视频', 2: '爱奇艺', 3: '优酷' },
+    source: [
+      { key: 1, name: '腾讯视频' },
+      { key: 2, name: '爱奇艺' },
+      { key: 3, name: '优酷' },
+      { key: 4, name: '芒果' },
+    ],
   },
-]);
+});
 
+const changeHotClass = () => {
+  loading.value = true;
+  hotList.value = [];
+  hotSource.value = 1;
+  getHotList();
+};
+
+//
+const changeHotSource = () => {
+  loading.value = true;
+  hotList.value = [];
+  getHotList();
+};
+
+// 获取数据
 const getHotList = async () => {
-  hotList.value = await zy.kuyunHot('2023-05-03', hotClass.value, hotSource.value);
-  console.log(hotList.value);
-  if (hotList.value.length) loading.value = false;
+  try {
+    const date = moment().subtract(1, 'days').format('YYYY-MM-DD');
+    hotList.value = await zy.kuyunHot(date, MODE_OPTIONS[hotClass.value].key, hotSource.value);
+    console.log(hotList.value);
+    if (hotList.value.length) loading.value = false;
+  } catch (err) {
+    MessagePlugin.warning(`error:${err}`);
+    console.error(err);
+  }
+};
+
+// 搜索资源
+const searchEvent = async (item) => {
+  const { key } = formData.value;
+  try {
+    MessagePlugin.info('请等待,正在搜索相关资源!');
+
+    const res = await zy.searchFirstDetail(key, item.vod_name);
+    formDetailData.value = res;
+    if (!res) {
+      MessagePlugin.warning('暂无在本源搜索到相关资源!');
+      return;
+    }
+
+    store.updateConfig({
+      type: 'film',
+      data: {
+        info: formDetailData.value,
+        ext: { site: formData.value },
+      },
+    });
+
+    if (formDetailData.value) ipcRenderer.send('openPlayWindow', item.vod_name);
+  } catch (err) {
+    console.error(err);
+    MessagePlugin.error('网络出错啦,请稍后再试!');
+  }
 };
 </script>
 
@@ -120,18 +229,15 @@ const getHotList = async () => {
       position: relative;
       flex-direction: row;
       align-items: center;
+      display: flex;
+      justify-content: space-between;
       .tab-item {
-        width: 65px;
-        position: relative;
-        color: #777;
-        text-align: center;
-        z-index: 1;
-        font-weight: 700;
-        user-select: none;
-      }
-      .active {
-        background-color: var(--td-bg-input);
-        border-radius: 5px;
+        display: flex;
+        flex-direction: row;
+        align-items: stretch;
+        .title {
+          margin-right: 5px;
+        }
       }
     }
   }
@@ -143,7 +249,7 @@ const getHotList = async () => {
     list-style: none;
     padding: 0;
     z-index: 1;
-    height: 400px;
+    height: 360px;
     overflow-x: hidden;
     overflow-y: scroll;
     .swiper-wrapper {
@@ -165,13 +271,16 @@ const getHotList = async () => {
           text-align: right;
           color: hsla(0, 0%, 40%, 0.6);
           font-weight: 700;
-          margin-right: 12px;
-          background-repeat: no-repeat;
-          margin-top: 2px;
+          margin: 2px 14px 0 10px;
         }
         .rank-1 {
           color: #f7534f;
-          padding-right: 2px;
+        }
+        .rank-2 {
+          color: #fa7b32;
+        }
+        .rank-3 {
+          color: #ffc63f;
         }
         .normal-view {
           flex-direction: row;
@@ -187,14 +296,33 @@ const getHotList = async () => {
             text-overflow: ellipsis;
             overflow: hidden;
           }
-          .normal-tip-icon {
-            border-radius: 2px;
-            text-align: center;
-            padding: 0.5px 3px;
+          .normal-tip {
             font-weight: 700;
           }
+          .color-1 {
+            color: #f7534f;
+          }
+          .color-2 {
+            color: #fa7b32;
+          }
+          .color-3 {
+            color: #ffc63f;
+          }
+        }
+        &:hover {
+          border-radius: 5px;
+          background-color: var(--td-bg-color-component-hover);
         }
       }
+    }
+  }
+  .tip-warp {
+    position: absolute;
+    left: calc(var(--td-comp-paddingLR-xxl));
+    bottom: calc(var(--td-comp-paddingTB-s));
+    span,
+    a {
+      font-size: var(--td-font-size-link-small);
     }
   }
 }
