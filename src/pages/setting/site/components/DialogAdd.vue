da2cb2b6eafb2118be76801393499299f42da70e
Hiram <wy-hai@qq.com>
2023-05-11 23:40:42 +0800

    add type

--- src/pages/setting/site/components/DialogAdd.vue
+++ src/pages/setting/site/components/DialogAdd.vue
@@ -19,6 +19,12 @@
         <t-form-item label="源站名" name="name">
           <t-input v-model="formData.siteInfo.name" class="input-item" placeholder="请输入内容" />
         </t-form-item>
+        <t-form-item label="类型" name="type">
+          <t-radio-group v-model="formData.siteInfo.type">
+            <t-radio :value="1">cms</t-radio>
+            <t-radio :value="2">dy</t-radio>
+          </t-radio-group>
+        </t-form-item>
         <t-form-item label="API接口" name="api">
           <t-input v-model="formData.siteInfo.api" class="input-item" placeholder="请输入内容" />
         </t-form-item>
