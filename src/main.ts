44c5542c791a85370d9044278d2cb6b857e80e82
Hiram <>
2023-09-25 00:39:54 +0800

    delete not import package

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,5 @@
-/* eslint-disable simple-import-sort/imports */
 import { createApp } from 'vue';
-import 'default-passive-events';
 
-// import TDesign from 'tdesign-vue-next';
 import App from './App.vue';
 import router from './router';
 import { store } from './store';
@@ -13,7 +10,6 @@ import './permission';
 
 const app = createApp(App);
 
-// app.use(TDesign);
 app.use(store);
 app.use(router);
 
