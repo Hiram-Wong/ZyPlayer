5806fb5967b1ecf83eba110b1255e3340b771f0c
Hiram <>
2023-12-03 17:39:21 +0800

    add i18

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import { store } from './store';
+import i18n from './locales';
 
 import 'tdesign-vue-next/es/style/index.css';
 import '@/style/index.less';
@@ -12,5 +13,6 @@ const app = createApp(App);
 
 app.use(store);
 app.use(router);
+app.use(i18n);
 
 app.mount('#app');
