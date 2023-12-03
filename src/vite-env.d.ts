07650946c736c52e171f1f036aea818379b1abc4
Hiram <>
2023-12-03 17:09:50 +0800

    delete useless file

--- src/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-
-// https://cn.vitejs.dev/guide/env-and-mode.html#env-files
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue';
-
-  const component: DefineComponent<{}, {}, any>;
-  export default component;
-}
