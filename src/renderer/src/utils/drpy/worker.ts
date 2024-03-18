d36db8c1ae81e134d2eaf830c0ed25c0677cfca8
Hiram <>
2024-03-18 23:57:31 +0800

    add drpy worker

---
+++ src/renderer/src/utils/drpy/worker.ts
@@ -0,0 +1,49 @@
+import { init, home, homeVod, category, detail, play, search, proxy, sniffer, isVideo } from './drpy3';
+
+const drpyWork = (parms) => {
+  const { type, data } = parms;
+  let res = { type, data };
+  switch (type) {
+    case 'init':
+      try {
+        init(data);
+        res.data = 'sucess';
+      } catch(_){
+        res.data = 'error';
+      }
+      break;
+    case 'home':
+      res.data = home();
+      break;
+    case 'homeVod':
+      res.data = homeVod();
+      break;
+    case 'category':
+      const { tid, pg: categoryPg, filter, extend } = data;
+      res.data = category(tid, categoryPg, filter, extend);
+      break;
+    case 'detail':
+      res.data = detail(data);
+      break;
+    case 'play':
+      const { flag, id, flags } = data;
+      res.data = play(flag, id, flags);
+      break;
+    case 'search':
+      const { wd, quick, pg: searchPg } = data;
+      res.data = search(wd, quick, searchPg);
+      break;
+    default:
+      break;
+  }
+  return res;
+};
+
+self.addEventListener(
+  'message',
+  (e) => {
+    const res = drpyWork(e.data);
+    self.postMessage(res);
+  },
+  false,
+);
