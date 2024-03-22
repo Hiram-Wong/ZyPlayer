d9a0d7f4c728cfa6cf2a992d815dd8ccd3df66ec
Hiram <>
2024-03-22 17:21:36 +0800

    add catvod add or edit entrance

--- src/renderer/src/pages/setting/site/components/DialogEdit.vue
+++ src/renderer/src/pages/setting/site/components/DialogEdit.vue
@@ -13,6 +13,7 @@
             <t-radio :value="2">drpy[js0]</t-radio>
             <t-radio :value="6">hipy[t4]</t-radio>
             <t-radio :value="7">js[t3]</t-radio>
+            <t-radio :value="8">catvod[nodejs]</t-radio>
             <t-radio :value="3">app[v3]</t-radio>
             <t-radio :value="4">app[v1]</t-radio>
             <!-- <t-radio :value="5">爬虫</t-radio> -->
