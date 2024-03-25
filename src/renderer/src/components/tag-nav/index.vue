abf7c2b6d33de3e041494dbc965ccd7378796f8f
Hiram <>
2024-03-25 18:56:06 +0800

    tabs style edit

--- src/renderer/src/components/tag-nav/index.vue
+++ src/renderer/src/components/tag-nav/index.vue
@@ -46,40 +46,4 @@ const handleItemClick = (key: string | number) => {
 .tag-nav {
   width: 100%;
 }
-
-:deep(.t-tabs__nav-container.t-is-top:after) {
-  background-color: rgba(0, 0, 0, 0) !important;
-}
-// :deep(.t-tabs__bar.t-is-top) {
-//   width:15px !important;
-//   left: 50% !important;
-//   transform: translate(-50%, 0);
-// }
-:deep(.t-tabs__nav-item) {
-  --ripple-color: transparent;
-  &:not(.t-is-disabled):not(.t-is-active):hover {
-    color: var(--td-brand-color);
-    .t-tabs__nav-item-wrapper {
-      background-color: transparent;
-    }
-  }
-}
-:deep(.t-tabs__nav-item-wrapper) {
-  --ripple-color: transparent;
-}
-:deep(.t-tabs__nav-item-text-wrapper) {
-  font-weight: 700;
-}
-:deep(.t-tabs__btn) {
-  border: 0;
-  background-color: var(--td-bg-color-container);
-  &:hover > svg {
-    color: var(--td-brand-color);
-  }
-}
-:deep(.t-tabs__operations--left), :deep(.t-tabs__operations--right) {
-  .t-tabs__btn:first-child {
-    box-shadow: none;
-  }
-}
 </style>
