4d6b01a17fc2133a509f5eaa2491b15f2738a0cf
Hiram <>
2024-04-16 15:36:34 +0800

    Fix data cannot be added when the key does not exist

--- src/main/core/server/routes/setting.ts
+++ src/main/core/server/routes/setting.ts
@@ -1,5 +1,6 @@
-import _ from 'lodash';
 import { FastifyReply, FastifyPluginAsync, FastifyRequest } from 'fastify';
+import _ from 'lodash';
+import { nanoid } from 'nanoid';
 
 import { setting } from '../../db/service';
 
@@ -33,16 +34,27 @@ const api: FastifyPluginAsync = async (fastify): Promise<void> => {
   })
   fastify.put(`/${API_VERSION}/setting`, async (req: FastifyRequest<{ Querystring: { [key: string]: string } }>, reply: FastifyReply) => {
     try {
-      const destination = req.body;
-      const source = await setting.source();
-
-      for (let i in destination) {
-        const index = _.findIndex(source, { key: i });
-        if (index !== -1) {
-          source[index]["value"] = destination[i];
+      const mergeCustomizer = (objValue, srcValue) => {
+        // PlainObject检查是否是由 Object 构造函数创建
+        if (_.isPlainObject(objValue) && _.isPlainObject(srcValue)) {
+          return _.mergeWith({}, objValue, srcValue, mergeCustomizer);
         }
-      }
-      const res = await setting.set(source);
+        return srcValue; // 保留源对象非对象属性的值
+      };
+
+      const destination = req.body || {};
+      const source = await setting.source();
+      const mergedSettings = _.mergeWith(_.cloneDeep(source), destination, mergeCustomizer);
+
+      // 添加具有生成ID的新设置项
+      const existingKeys = source.map((item) => item.key);
+      Object.entries(destination).forEach(([key, value]) => {
+        if (!existingKeys.includes(key)) {
+          mergedSettings.push({ key, value, id: nanoid() });
+        }
+      });
+
+      const res = await setting.set(mergedSettings);
       reply.code(200).send(res);
     } catch (err) {
       reply.code(500).send(err)
