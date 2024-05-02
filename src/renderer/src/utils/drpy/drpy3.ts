dabf9bb5c836eb0efe7c2b5130ea317507fbdb49
晚风拂柳颜 <434857005@qq.com>
2024-05-02 09:47:24 +0800

    改版本号

--- src/renderer/src/utils/drpy/drpy3.ts
+++ src/renderer/src/utils/drpy/drpy3.ts
@@ -92,7 +92,7 @@ const pre = () => {
 let rule = {};
 // @ts-ignore
 let vercode = typeof pdfl === 'function' ? 'drpy3.1' : 'drpy3';
-const VERSION = `${vercode} 3.9.50beta4 202400501`;
+const VERSION = `${vercode} 3.9.50beta5 202400502`;
 /** 已知问题记录
  * 1.影魔的jinjia2引擎不支持 {{fl}}对象直接渲染 (有能力解决的话尽量解决下，支持对象直接渲染字符串转义,如果加了|safe就不转义)[影魔牛逼，最新的文件发现这问题已经解决了]
  * Array.prototype.append = Array.prototype.push; 这种js执行后有毛病,for in 循环列表会把属性给打印出来 (这个大毛病需要重点排除一下)
