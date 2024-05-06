c97a2f98398254af9ba1d7aeb290e874ad5ef9c4
Hiram <>
2024-05-06 15:02:32 +0800

    support server config generate

--- src/main/core/server/routes/file.ts
+++ src/main/core/server/routes/file.ts
@@ -1,10 +1,11 @@
 import { FastifyReply, FastifyPluginAsync, FastifyRequest } from 'fastify';
 import fs from 'fs-extra';
 import { app } from 'electron';
-import { join } from "path";
+import { nanoid } from 'nanoid';
+import { join, relative, basename, extname } from "path";
 
 const API_VERSION = "api/v1";
-const BASE_PATH = join(app.getPath("userData"), 'file'); // 文件路径 
+const BASE_PATH = join(app.getPath("userData"), 'file'); // 文件路径
 
 const api: FastifyPluginAsync = async (fastify): Promise<void> => {
   fastify.delete(`/${API_VERSION}/file/:filename(.*)`, async (req: FastifyRequest<{ Querystring: { [key: string]: string } }>, reply: FastifyReply) => {
@@ -37,6 +38,48 @@ const api: FastifyPluginAsync = async (fastify): Promise<void> => {
       } else {
         reply.code(404).send({ error: 'File not found' });
       }
+    } catch (err) {
+      reply.code(500).send(err)
+    }
+  });
+  fastify.get(`/${API_VERSION}/file/config`, async (_, reply: FastifyReply) => {
+    try {
+      const doc: any = {
+        sites: []
+      };
+
+      const walk = async (directoryPath: string, rootDirectory: string) => {
+        const files = await fs.readdir(directoryPath);
+
+        for (const file of files) {
+          const filePath = join(directoryPath, file);
+          const stats = await fs.stat(filePath);
+
+          if (stats.isDirectory()) {
+            await walk(filePath, rootDirectory);
+          } else {
+            if (/\.js(?:\?.*)?$/.test(filePath)) {
+              const relativePath = relative(rootDirectory, filePath)
+              const fileName = basename(relativePath, extname(relativePath));
+
+              doc.sites.push({
+                key: nanoid(),
+                name: fileName,
+                type: 3,
+                api: `http://127.0.0.1:9978/api/v1/file/${relativePath}`,
+                searchable: 1,
+                quickSearch: 0,
+                filterable: 1,
+                ext: `./${relativePath}`
+              });
+            }
+          }
+        }
+      }
+
+      await walk(BASE_PATH, BASE_PATH);
+
+      reply.code(200).send(doc)
     } catch (err) {
       reply.code(500).send(err)
     }
