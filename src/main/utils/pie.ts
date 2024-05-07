44f1d57891ae550900cad70c17b2e58ce408c1d3
Hiram <>
2024-05-08 01:33:17 +0800

    optimize sniffing logic

--- src/main/utils/pie.ts
+++ src/main/utils/pie.ts
@@ -1,45 +1,35 @@
 import { app, BrowserWindow } from 'electron';
+import { nanoid } from 'nanoid';
 import puppeteer from 'puppeteer-core';
 import pie from 'puppeteer-in-electron';
 import logger from '../core/logger';
-import { v4 as uuidv4 } from 'uuid';
 
 interface PieResponse {
   code: number;
-  message: 'sucess' | 'fail';
-  data: object | Error;
+  message: 'success' | 'fail';
+  data: any;
 }
 
 pie.initialize(app);
 let snifferWindow: BrowserWindow;
-const urlRegex: RegExp = new RegExp('http((?!http).){12,}?\\.(m3u8|mp4|flv|avi|mkv|rm|wmv|mpg|m4a|mp3)\\?.*|http((?!http).){12,}\\.(m3u8|mp4|flv|avi|mkv|rm|wmv|mpg|m4a|mp3)|http((?!http).)*?video/tos*');
+
+const urlRegex: RegExp = new RegExp('http((?!http).){12,}?\\.(m3u8|mp4|flv|avi|mkv|rm|wmv|mpg|m4a|mp3|tos)\\?.*|http((?!http).){12,}\\.(m3u8|mp4|flv|avi|mkv|rm|wmv|mpg|m4a|mp3)|http((?!http).)*?video/tos*');
 const pageStore: object = {};
 
-const handleResponse = (code: number, message: 'sucess' | 'fail', data: object | Error, pageId: string): PieResponse => {
-  if (pageId) {
-    if (pageStore[pageId].timerId) {
-      clearTimeout(pageStore[pageId].timerId);
-    }
-    delete pageStore[pageId];
-  }
-  let dataString;
+const handleResponse = (code: number, message: 'success' | 'fail', data: object | Error): PieResponse => {
+  let dataString: any;
   if (typeof data === 'object') {
     dataString = JSON.stringify(data);
   } else {
     dataString = data;
   }
   logger.info(`[sniffer] code: ${code} - message: ${message} - data: ${dataString}`);
-  return ({ code: code, message: message, data });
+  return ({ code, message, data });
 };
 
 // 排除url
-const isExcludedUrl = (reqUrl) => {
-  return (
-    reqUrl.indexOf('url=http') >= 0 ||
-    reqUrl.indexOf('v=http') >= 0 ||
-    reqUrl.indexOf('.css') >= 0 ||
-    reqUrl.indexOf('.html') >= 0
-  );
+const isExcludedUrl = (reqUrl: string): boolean => {
+  return /\.(css|html)$/.test(reqUrl) || /(url=http|v=http)/i.test(reqUrl);
 };
 
 // 视频url
@@ -49,75 +39,85 @@ const isVideoUrl = (reqUrl) => {
 
 const puppeteerInElectron = async (url: string, script: string = '', customRegex: string, ua: string | null = null): Promise<PieResponse> => {
   logger.info(`[sniffer] sniffer url: ${url}`);
-  logger.info(`[sniffer] sniffer script: ${script}`);
   logger.info(`[sniffer] sniffer ua: ${ua}`);
-  let pageId = ''; // 如果窗口没起来就空的页面id
+
+  const pageId = nanoid(); // 生成page页面id
+
   try {
     const browser = await pie.connect(app, puppeteer as any); // 连接puppeteer
     snifferWindow = new BrowserWindow({ show: false }); // 创建无界面窗口
     snifferWindow.webContents.setAudioMuted(true); // 设置窗口静音
+
     const page = await pie.getPage(browser, snifferWindow); // 获取页面
-    pageId = uuidv4(); // 生成page页面id
-    pageStore[pageId] = { page: page, time: new Date().getTime() / 1000, timerId: null }; // 存储页面
+    const pageRecord = { page, browser, timestamp: Date.now() / 1000, timeoutId: null };
+    pageStore[pageId] = pageRecord; // 存储页面
+
     if (ua) await page.setUserAgent(ua); // 设置ua
     await page.setRequestInterception(true); // 开启请求拦截
 
     return new Promise(async (resolve, reject) => {
+      const cleanup = async (pageId: string) => {
+        if (pageId) {
+          if (pageStore[pageId]) {
+            if (pageStore[pageId]?.timeoutId) clearTimeout(pageStore[pageId].timeoutId);
+            if (pageStore[pageId]?.page) await pageStore[pageId].page.close().catch(err => logger.error(err));
+            if (pageStore[pageId]?.browser) await pageStore[pageId].browser.disconnect();
+            delete pageStore[pageId];
+          }
+        };
+      };
+
       page.on('request', async (req) => {
+        if (req.isInterceptResolutionHandled()) return; // 已处理过的请求不再处理
+
         const reqUrl = req.url(); // 请求url
-        const reqHeaders = req.headers();
+        const reqHeaders = req.headers(); // 请求头
         const { referer, 'user-agent': userAgent } = reqHeaders;
         const headers = {};
         if (referer) headers['referer'] = referer;
         if (userAgent) headers['user-agent'] = userAgent;
 
         if (customRegex && reqUrl.match(new RegExp(customRegex, 'gi'))) {
-          logger.info(`[pie]通过custom_regex嗅探到真实地址:${reqUrl}`);
-          await page.close();
-          await browser.disconnect();
+          logger.info(`[pie]正则匹配:${reqUrl}`);
+          await cleanup(pageId);
           req.abort().catch((e) => logger.error(e));
-          resolve(handleResponse(200, 'sucess', { url: reqUrl, header: headers }, pageId));
-        }
+          resolve(handleResponse(200, 'success', { url: reqUrl, header: headers }));
+        };
 
         if (isVideoUrl(reqUrl)) {
-          logger.info(`[pie]后缀名匹配嗅探到真实地址:${reqUrl}`);
-          await page.close();
-          await browser.disconnect();
+          logger.info(`[pie]后缀名匹配:${reqUrl}`);
+          await cleanup(pageId);
           req.abort().catch((e) => logger.error(e));
-          resolve(handleResponse(200, 'sucess', { url: reqUrl, header: headers }, pageId));
-        }
+          resolve(handleResponse(200, 'success', { url: reqUrl, header: headers }));
+        };
 
-        if (req.isInterceptResolutionHandled()) return; // 已处理过的请求不再处理
         if (req.method().toLowerCase() === 'head') {
           req.abort().catch((err) => logger.error(err));
-        }
-        const resourceTypesToAbort = ['font'];
-        if (resourceTypesToAbort.includes(req.resourceType())) {
+        };
+
+        if (['font'].includes(req.resourceType())) {
           req.abort().catch((err) => logger.error(err));
-        } else {
-          req.continue().catch((err) => logger.error(err));
-        }
+        };
+
+        req.continue().catch((err) => logger.error(err));
       });
 
       // 设置超时
       if (!pageStore[pageId].timerId) {
         logger.info('--------!timerId---------');
         pageStore[pageId].timerId = setTimeout(async () => {
-          await page.close();
-          await browser.disconnect();
-          pageStore[pageId].timerId = null;
-          reject(new Error('嗅探失败', { cause: '请求超时' }));
+          await cleanup(pageId);
+          reject(handleResponse(500, 'fail', new Error('fail', { cause: 'sniffer timeout' })));
         }, 15000);
       } else {
-        logger.info('--------has timerId---------');
-      }
+        logger.info('--------has timerId------');
+      };
 
+      await page.goto(url, { waitUntil: 'domcontentloaded' }).catch((err) => logger.error(err));
 
-      await page.goto(url, { waitUntil: 'domcontentloaded' }).catch(err => reject(err));
-      logger.info('----------waitUntil---------------');
       if (script.trim()) {
         try {
-          logger.info(`[pie]start run script`);
+          logger.info(`[sniffer] sniffer script: ${script}`);
           const js_code = `
             (function() {
               var scriptTimer;
@@ -139,10 +139,10 @@ const puppeteerInElectron = async (url: string, script: string = '', customRegex
         } catch (err) {
           logger.info(`[pie][error]run script: ${err}`);
         }
-      }
+      };
     });
   } catch (err) {
-    return handleResponse(500, 'fail', err as Error, pageId);
+    return handleResponse(500, 'fail', err as Error);
   }
 };
 
