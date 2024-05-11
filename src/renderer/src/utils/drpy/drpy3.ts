11d1886274053630e81113e8a4dee48675504477
晚风拂柳颜 <434857005@qq.com>
2024-05-12 04:05:30 +0800

    修改版本号.修正二级没try的问题

--- src/renderer/src/utils/drpy/drpy3.ts
+++ src/renderer/src/utils/drpy/drpy3.ts
@@ -91,7 +91,7 @@ const pre = () => {
 let rule = {};
 // @ts-ignore
 let vercode = typeof pdfl === 'function' ? 'drpy3.1' : 'drpy3';
-const VERSION = `${vercode} 3.9.50beta10 202400510`;
+const VERSION = `${vercode} 3.9.50beta11 202400512`;
 /** 已知问题记录
  * 1.影魔的jinjia2引擎不支持 {{fl}}对象直接渲染 (有能力解决的话尽量解决下，支持对象直接渲染字符串转义,如果加了|safe就不转义)[影魔牛逼，最新的文件发现这问题已经解决了]
  * Array.prototype.append = Array.prototype.push; 这种js执行后有毛病,for in 循环列表会把属性给打印出来 (这个大毛病需要重点排除一下)
@@ -2493,7 +2493,11 @@ const detailParse = (detailObj) => {
   }
   let t2 = Date.now();
   console.log(`[t3]加载二级界面${MY_URL}耗时:${t2 - t1}毫秒`);
-  vod = vodDeal(vod);
+  try {
+    vod = vodDeal(vod);
+  }catch (e) {
+    console.log(`[t3]vodDeal发生错误:${e.message}`);
+  }
 
   return {
     list: [vod],
