da6107a80791ff4b5e84c2ee3bfc710809aa2bfe
Hiram <>
2024-06-01 01:23:31 +0800

    [bugs] pinia status missing

--- src/renderer/src/store/index.ts
+++ src/renderer/src/store/index.ts
@@ -9,7 +9,7 @@ store.use(
     // If set to true this tab tries to immediately recover the shared state from another tab. Defaults to true.
     initialize: true,
     // Enforce a type. One of native, idb, localstorage or node. Defaults to native.
-    type: 'localstorage',
+    type: window.BroadcastChannel ? 'native' : 'localstorage',
   }),
 );
 
