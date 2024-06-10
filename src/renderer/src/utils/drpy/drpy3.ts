e2b06ac33e2c64e42a3fe6aefbb6863404115aec
晚风拂柳颜 <434857005@qq.com>
2024-06-10 11:07:08 +0800

    修复一处lazy结果识别parse为0和1可能不准确的问题

--- src/renderer/src/utils/drpy/drpy3.ts
+++ src/renderer/src/utils/drpy/drpy3.ts
@@ -267,7 +267,7 @@ const pre = () => {
 let rule = {};
 // @ts-ignore
 let vercode = typeof pdfl === 'function' ? 'drpy3.1' : 'drpy3';
-const VERSION = `${vercode} 3.9.50beta27 20240608`;
+const VERSION = `${vercode} 3.9.50beta28 20240610`;
 /** 已知问题记录
  * 1.影魔的jinjia2引擎不支持 {{fl}}对象直接渲染 (有能力解决的话尽量解决下，支持对象直接渲染字符串转义,如果加了|safe就不转义)[影魔牛逼，最新的文件发现这问题已经解决了]
  * Array.prototype.append = Array.prototype.push; 这种js执行后有毛病,for in 循环列表会把属性给打印出来 (这个大毛病需要重点排除一下)
@@ -2971,7 +2971,7 @@ const playParse = (playObj) => {
         typeof input === 'object'
           ? input
           : {
-            parse: 1,
+            parse: SPECIAL_URL.test(input) || /^(push:)/.test(input) ? 0 : 1,
             jx: tellIsJx(input),
             url: input,
           };
