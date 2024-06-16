1fde621413d50064c88ca0a38d76ccda769141b6
晚风拂柳颜 <434857005@qq.com>
2024-06-16 17:07:06 +0800

    调整drpy接口的gzip可能表现不一致问题

--- src/renderer/src/utils/drpy/drpy3.ts
+++ src/renderer/src/utils/drpy/drpy3.ts
@@ -267,7 +267,7 @@ const pre = () => {
 let rule = {};
 // @ts-ignore
 let vercode = typeof pdfl === 'function' ? 'drpy3.1' : 'drpy3';
-const VERSION = `${vercode} 3.9.50beta29 20240613`;
+const VERSION = `${vercode} 3.9.50beta30 20240616`;
 /** 已知问题记录
  * 1.影魔的jinjia2引擎不支持 {{fl}}对象直接渲染 (有能力解决的话尽量解决下，支持对象直接渲染字符串转义,如果加了|safe就不转义)[影魔牛逼，最新的文件发现这问题已经解决了]
  * Array.prototype.append = Array.prototype.push; 这种js执行后有毛病,for in 循环列表会把属性给打印出来 (这个大毛病需要重点排除一下)
@@ -939,7 +939,7 @@ const Utf8ArrayToStr = (array: Uint8Array) => {
  */
 const gzip = (str) => {
   const arr = pako.gzip(str, {
-    to: 'string',
+    // to: 'string',
   });
   return uint8ArrayToBase64(arr);
 };
