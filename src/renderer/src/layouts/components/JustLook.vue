c2fa2c8007a359af6daafb30ae182e9f472a8faf
Hiram <>
2024-07-09 19:26:57 +0800

    [optimization] support keyboard change video && drawer min width && style && video dynamic background

--- src/renderer/src/layouts/components/JustLook.vue
+++ src/renderer/src/layouts/components/JustLook.vue
@@ -3,14 +3,15 @@
     <t-button theme="default" shape="square" variant="text" @click="toggleDrawerAndHandlePlayer">
       <dvd-icon />
     </t-button>
-    <t-drawer v-model:visible="isVisible.drawer" size-draggable show-in-attached-element :footer=null
-      :attach="`.${prefix}-content`" @confirm="change" @close="close" size="320px">
+    <t-drawer v-model:visible="isVisible.drawer" show-in-attached-element :footer=null :attach="`.${prefix}-content`"
+      :size-draggable="{ max: 920, min: 320 }" @confirm="change" @close="close" size="320px">
       <div class="content">
         <div id="mse"></div>
-        <t-button shape="round" theme="default" variant="dashed" class="btn" @click="change">
-          <template #icon><refresh-icon /></template>
-          <span>{{ $t('pages.justlook.confirm') }}</span>
-        </t-button>
+        <div class="slide">
+          <div class="icon refresh" @click="change">
+            <refresh-icon />
+          </div>
+        </div>
       </div>
     </t-drawer>
   </div>
@@ -41,6 +42,25 @@ const config = ref({
     clockwise: false,
     innerRotate: true,
     index: 1
+  },
+  keyboard: {
+    keyCodeMap: {
+      'up': {
+        action: function () {
+          console.log('[justlook][keyboard]up fresh video')
+          change();
+        }
+      },
+      'down': {
+        action: function () {
+          console.log('[justlook][keyboard]down fresh video')
+          change();
+        }
+      }
+    }
+  },
+  dynamicBg: {
+    disable: false
   },
   ignores: ['cssFullscreen', 'playbackRate'],
   height: '100%',
@@ -98,14 +118,42 @@ const change = () => {
   justify-content: space-between;
   height: 100%;
 
-  #player {
+  #mse {
     height: 100%;
     border-radius: var(--td-radius-large);
   }
 
-  .btn {
-    margin-top: var(--td-comp-margin-xs);
-    width: 100%;
+  .slide {
+    display: flex;
+    flex-direction: column;
+    position: absolute;
+    right: calc(12px + var(--td-comp-paddingLR-l));
+    top: calc(22px + var(--td-comp-paddingTB-l));
+    width: 32px;
+    z-index: 10;
+
+    .icon {
+      background-color: #33343f;
+      height: 40px;
+      line-height: 40px;
+      opacity: .7;
+      text-align: center;
+      width: 32px;
+
+      &:hover {
+        background-color: #4e5255;
+        cursor: pointer;
+      }
+
+      svg {
+        color: #fff;
+      }
+    }
+
+    .refresh {
+      border-radius: var(--td-radius-round);
+
+    }
   }
 }
 </style>
