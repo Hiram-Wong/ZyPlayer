b546db4bf34559442a1efa3a70b5e1c2cf390102
Hiram <>
2024-08-30 13:40:26 +0800

    [optimize] new db data migration logic

--- src/main/core/db/magrite/index.ts
+++ src/main/core/db/magrite/index.ts
@@ -1,49 +1,36 @@
 import db from '../index';
 import logger from '../../logger';
-import {
-  update3_3_1_to3_3_2,
-  update3_3_3_to3_3_4,
-  update3_3_4_to3_3_5,
-  update3_3_6_to3_3_7,
-} from './data';
+import { update3_3_1_to3_3_2, update3_3_3_to3_3_4, update3_3_4_to3_3_5, update3_3_6_to3_3_7 } from './data';
+import { compare } from 'compare-versions';
 import { app } from 'electron';
 
-/**
- * 判断版本
- * @param versionA '3.3.3'
- * @param versionB '3.3.4'
- * @returns 1:大 -1小 0相等
- */
-const compareVersion = (versionA: string, versionB: string) => {
-  const partsA = versionA.split('.').map(Number);
-  const partsB = versionB.split('.').map(Number);
+const updates = [
+  { version: '3.3.2', update: update3_3_1_to3_3_2 },
+  { version: '3.3.4', update: update3_3_3_to3_3_4 },
+  { version: '3.3.5', update: update3_3_4_to3_3_5 },
+  { version: '3.3.7', update: update3_3_6_to3_3_7 },
+];
 
-  // Compare parts from left to right
-  for (let i = 0; i < Math.max(partsA.length, partsB.length); i++) {
-    const partA = partsA[i] || 0;
-    const partB = partsB[i] || 0;
+const magrite = () => {
+  let dbVersion = db.get('tbl_setting').find({ key: 'version' }).value()?.value ?? '0.0.0';
+  const appVersion = app.getVersion();
+  logger.info(`[db][magrite][version] db:${dbVersion} - app:${appVersion}`);
 
-    if (partA !== partB) {
-      return partA > partB ? -1 : 1;
+  if (compare(dbVersion, appVersion, '<')) {
+    for (const { version, update } of updates) {
+      if (compare(dbVersion, version, '<')) {
+        try {
+          update();
+          dbVersion = version;
+          logger.info(`[db][magrite] ${version} success`);
+        } catch (err) {
+          logger.error(`[db][magrite] ${version} failed`, err);
+        }
+      }
     }
   }
 
-  return 0;
-};
-
-const magrite = () => {
-  const currentVersion = db.get('tbl_setting').find({ key: 'version' }).value()?.value ?? '';
-  const lastVersion = app.getVersion();
-  logger.info(`[db][magrite][version][current:${currentVersion}][last:${lastVersion}]`);
-
-  if (compareVersion(currentVersion, lastVersion) === 1) {
-    if (!currentVersion) update3_3_1_to3_3_2();
-    if (compareVersion(currentVersion, '3.3.4') === 1) update3_3_3_to3_3_4();
-    if (compareVersion(currentVersion, '3.3.5') === 1) update3_3_4_to3_3_5();
-    if (compareVersion(currentVersion, '3.3.7') === 1) update3_3_6_to3_3_7();
-  }
-
-  logger.info(`[db][magrite]magrite completed`);
+  logger.info(`[db][magrite] completed`);
 };
 
 export default magrite;
