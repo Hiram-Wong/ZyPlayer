0e076961f52f95c3164109abaae1d3645ae54c06
Hiram <>
2024-11-19 23:04:52 +0800

    [optimize] atob btoa logic

--- src/main/utils/crypto/index.ts
+++ src/main/utils/crypto/index.ts
@@ -4,21 +4,206 @@ import * as he from 'he';
 import pako from 'pako';
 import WxmpRsa from 'wxmp-rsa';
 
-const base64 = {
-  decode: (val: string) => crypto.enc.Utf8.stringify(crypto.enc.Base64.parse(val)),
-  encode: (val: string) => crypto.enc.Base64.stringify(crypto.enc.Utf8.parse(val)),
-  btoa: (val: string) => {
-    const isAllAscii = (val: string) => {
-      return val.split('').every((char) => {
-        const code = char.charCodeAt(0);
-        return code >= 0 && code <= 127;
-      });
-    };
-    if (!isAllAscii(val)) return '';
-    return crypto.enc.Base64.stringify(crypto.enc.Utf8.parse(val));
-  },
-  atob: (val: string) => crypto.enc.Utf8.stringify(crypto.enc.Base64.parse(val)),
-};
+const base64 = (() => {
+  const b64map = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+  const base64DecodeChars = new Array(
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    62,
+    -1,
+    -1,
+    -1,
+    63,
+    52,
+    53,
+    54,
+    55,
+    56,
+    57,
+    58,
+    59,
+    60,
+    61,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+    16,
+    17,
+    18,
+    19,
+    20,
+    21,
+    22,
+    23,
+    24,
+    25,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    26,
+    27,
+    28,
+    29,
+    30,
+    31,
+    32,
+    33,
+    34,
+    35,
+    36,
+    37,
+    38,
+    39,
+    40,
+    41,
+    42,
+    43,
+    44,
+    45,
+    46,
+    47,
+    48,
+    49,
+    50,
+    51,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+  );
+  return {
+    decode: (val: string) => crypto.enc.Utf8.stringify(crypto.enc.Base64.parse(val)),
+    encode: (val: string) => crypto.enc.Base64.stringify(crypto.enc.Utf8.parse(val)),
+    btoa: (val: string) => {
+      var out, i, len;
+      var c1, c2, c3;
+      len = val.length;
+      i = 0;
+      out = '';
+      while (i < len) {
+        c1 = val.charCodeAt(i++) & 0xff;
+        if (i == len) {
+          out += b64map.charAt(c1 >> 2);
+          out += b64map.charAt((c1 & 0x3) << 4);
+          out += '==';
+          break;
+        }
+        c2 = val.charCodeAt(i++);
+        if (i == len) {
+          out += b64map.charAt(c1 >> 2);
+          out += b64map.charAt(((c1 & 0x3) << 4) | ((c2 & 0xf0) >> 4));
+          out += b64map.charAt((c2 & 0xf) << 2);
+          out += '=';
+          break;
+        }
+        c3 = val.charCodeAt(i++);
+        out += b64map.charAt(c1 >> 2);
+        out += b64map.charAt(((c1 & 0x3) << 4) | ((c2 & 0xf0) >> 4));
+        out += b64map.charAt(((c2 & 0xf) << 2) | ((c3 & 0xc0) >> 6));
+        out += b64map.charAt(c3 & 0x3f);
+      }
+      return out;
+    },
+    atob: (val: string) => {
+      var c1, c2, c3, c4;
+      var i, len, out;
+      len = val.length;
+      i = 0;
+      out = '';
+      while (i < len) {
+        do {
+          c1 = base64DecodeChars[val.charCodeAt(i++) & 0xff];
+        } while (i < len && c1 == -1);
+        if (c1 == -1) break;
+        do {
+          c2 = base64DecodeChars[val.charCodeAt(i++) & 0xff];
+        } while (i < len && c2 == -1);
+        if (c2 == -1) break;
+        out += String.fromCharCode((c1 << 2) | ((c2 & 0x30) >> 4));
+        do {
+          c3 = val.charCodeAt(i++) & 0xff;
+          if (c3 == 61) return out;
+          c3 = base64DecodeChars[c3];
+        } while (i < len && c3 == -1);
+        if (c3 == -1) break;
+        out += String.fromCharCode(((c2 & 0xf) << 4) | ((c3 & 0x3c) >> 2));
+        do {
+          c4 = val.charCodeAt(i++) & 0xff;
+          if (c4 == 61) return out;
+          c4 = base64DecodeChars[c4];
+        } while (i < len && c4 == -1);
+        if (c4 == -1) break;
+        out += String.fromCharCode(((c3 & 0x03) << 6) | c4);
+      }
+      return out;
+    },
+  };
+})();
 
 const hash = {
   'md5-16': (val: string) => crypto.MD5(val).toString().substr(8, 16),
