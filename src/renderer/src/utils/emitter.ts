adb8bd2fc2be2fdf24c00850c4a6146fd8269fe4
Hiram <>
2025-01-25 19:49:33 +0800

    [change] emitter ts type check

--- src/renderer/src/utils/emitter.ts
+++ src/renderer/src/utils/emitter.ts
@@ -1,12 +1,4 @@
 import mitt from "mitt";
 
-type Events = {
-  to: string
-  gallery: string
-  tool: string
-  pathname: string
-  extract: string
-}
-
-const emitter = mitt<Events>();
+const emitter = mitt();
 export default emitter;
