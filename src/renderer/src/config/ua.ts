a97b73d6f0160ba5ed13ed082f5951b221d7455e
Hiram <wy-hai@qq.com>
2025-04-28 01:32:06 +0800

    [change] default ua

--- src/renderer/src/config/ua.ts
+++ src/renderer/src/config/ua.ts
@@ -3,28 +3,44 @@ export default [
     name: 'Chrome',
     ua: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36',
   },
-  {
-    name: 'Edge',
-    ua: 'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Edg/112.0.1722.34',
-  },
+  // {
+  //   name: 'Edge',
+  //   ua: 'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Edg/112.0.1722.34',
+  // },
   {
     name: 'Safari',
     ua: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.5 Safari/605.1.15',
   },
+  // {
+  //   name: 'Firefox',
+  //   ua: 'Mozilla/5.0 (Windows NT 5.1; rv:5.0) Gecko/20100101 Firefox/5.0',
+  // },
+  // {
+  //   name: 'MOBILE',
+  //   ua: 'Mozilla/5.0 (Linux; Android 11; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.91 Mobile Safari/537.36',
+  // },
+  // {
+  //   name: 'UC',
+  //   ua: 'Mozilla/5.0 (Linux; U; Android 9; zh-CN; MI 9 Build/PKQ1.181121.001) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/57.0.2987.108 UCBrowser/12.5.5.1035 Mobile Safari/537.36',
+  // },
   {
-    name: 'Firefox',
-    ua: 'Mozilla/5.0 (Windows NT 5.1; rv:5.0) Gecko/20100101 Firefox/5.0',
-  },
-  {
-    name: 'MOBILE',
-    ua: 'Mozilla/5.0 (Linux; Android 11; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.91 Mobile Safari/537.36',
-  },
-  {
-    name: 'UC',
+    name: 'Android',
     ua: 'Mozilla/5.0 (Linux; U; Android 9; zh-CN; MI 9 Build/PKQ1.181121.001) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/57.0.2987.108 UCBrowser/12.5.5.1035 Mobile Safari/537.36',
   },
   {
-    name: 'IOS',
+    name: 'IPhone',
     ua: 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
+  },
+  {
+    name: 'Mpv',
+    ua: 'libmpv',
+  },
+  {
+    name: 'OkHttp',
+    ua: 'okhttp/4.9.1',
+  },
+  {
+    name: 'Dart',
+    ua: 'Dart/2.14 (dart:io)',
   },
 ];
