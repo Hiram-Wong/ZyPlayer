8eef876b87e2907ce9e938c06b1b618757bfad9a
startgo <50304042+ystartgo@users.noreply.github.com>
2025-09-04 03:38:25 +0800

    Create film.ts

---
+++ src/renderer/src/locales/lang/zh_TW/pages/film.ts
@@ -0,0 +1,23 @@
+export default {
+  name: '影視',
+  noDesc: '暫無劇情簡介',
+  info: {
+    unknown: '未知',
+    type: '類型',
+    area: '地區',
+    release: '上映',
+  },
+  infiniteLoading: {
+    complete: '已經到底線了',
+    error: '哎呀，出了一點差錯',
+    noMore: '已經到底線了',
+    noData: '暫無資料，請前往 [設定->影視配置] 配置資料',
+    networkError: '網路請求失敗，請嘗試手動重新整理',
+    categoryError: '設定分類異常，請前往設定檢查來源分類後再手動重新整理',
+  },
+  message: {
+    formatSeasonError: '資料來源格式化劇集錯誤，請確認資料來源是否正常',
+    notSelectAnalyze: '已識別到官方解析資料，但尚未選擇解析線路',
+    notSelectSourceBeforeSearch: '未選擇資料來源，或本站模式中該資料來源的搜尋狀態已關閉',
+  },
+};
