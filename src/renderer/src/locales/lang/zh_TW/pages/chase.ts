637b514424f16f5321d0b18770e6466e17d8dc41
startgo <50304042+ystartgo@users.noreply.github.com>
2025-09-04 03:36:05 +0800

    Create chase.ts

---
+++ src/renderer/src/locales/lang/zh_TW/pages/chase.ts
@@ -0,0 +1,46 @@
+export default {
+  name: '時刻',
+  history: {
+    clearAll: '清空',
+    title: '歷史',
+  },
+  type: {
+    film: '影視',
+    iptv: '電視',
+    drive: '網盤',
+    analyze: '解析',
+  },
+  binge: {
+    title: '收藏',
+    checkUpdate: '更新',
+    isUpdate: '有更新',
+    clearAll: '清空',
+    message: {
+      noCheckData: '無檢測資料，跳過此操作',
+    },
+  },
+  dialog: {
+    clearAll: {
+      header: '刪除記錄',
+      body: '確定要刪除所有記錄嗎？刪除後無法復原。',
+    },
+    cancel: '取消',
+    confirm: '確定',
+  },
+  date: {
+    today: '今天',
+    week: '七天內',
+    ago: '更早',
+  },
+  progress: {
+    watched: '已看完',
+    watching: '已看',
+  },
+  infiniteLoading: {
+    complete: '已經到底線了',
+    noMore: '已經到底線了',
+    error: '哎呀，出了一點差錯',
+  },
+  sourceDeleted: '該來源應該已被刪除了喔',
+  reqError: '請求資源站失敗，請檢查網路',
+};
