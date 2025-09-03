55645b3360be29dc72e50db2f4e9363122aa90ea
startgo <50304042+ystartgo@users.noreply.github.com>
2025-09-04 03:49:05 +0800

    Create share.ts

---
+++ src/renderer/src/locales/lang/zh_TW/pages/share.ts
@@ -0,0 +1,12 @@
+export default {
+  headerName: '掃一掃，手機繼續看',
+  headerInfoRecommend: '推薦',
+  headerInfoBrowser: '夸克 APP',
+  headerInfoScan: '-相機-掃碼',
+  headerCopyright: '提供支援，嚴禁傳播資源',
+  copyUrl: '複製網址',
+  message: {
+    copySuccess: '複製成功，快分享給好友吧！',
+    copyFail: '當前環境不支援一鍵複製，請手動複製連結！'
+  }
+};
