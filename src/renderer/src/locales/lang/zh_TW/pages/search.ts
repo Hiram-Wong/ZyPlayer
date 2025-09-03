3f5a7111e9f3fd26ea9be9a7c951fadb2e086e2f
startgo <50304042+ystartgo@users.noreply.github.com>
2025-09-04 03:46:47 +0800

    Create search.ts

---
+++ src/renderer/src/locales/lang/zh_TW/pages/search.ts
@@ -0,0 +1,11 @@
+export default {
+  film: '影視',
+  iptv: '電視',
+  site: '本站',
+  group: '組內',
+  all: '全站',
+  searchHistory: '搜尋歷史',
+  searchSource: '搜尋來源',
+  searchPlaceholder: '搜尋全網資源',
+  hotNoData: '暫無最近三天資料，請查看其他分類',
+};
