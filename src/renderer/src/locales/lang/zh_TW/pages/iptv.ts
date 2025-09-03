1814db2fe1b3f23b06d74bf725f05d7d794a3aa4
startgo <50304042+ystartgo@users.noreply.github.com>
2025-09-04 03:40:27 +0800

    Create iptv.ts

---
+++ src/renderer/src/locales/lang/zh_TW/pages/iptv.ts
@@ -0,0 +1,22 @@
+export default {
+  name: '電視',
+  delay: '超時',
+  unknown: '未知',
+  infiniteLoading: {
+    complete: '已經到底線了',
+    error: '哎呀，出了一點差錯',
+    noMore: '已經到底線了',
+    noData: '暫無資料，請前往 [設定->電視配置] 配置資料',
+    loading: '載入中...',
+  },
+  contextMenu: {
+    copyChannel: '複製頻道',
+    delChannel: '刪除頻道',
+  },
+  message: {
+    setSuccess: '設定成功',
+    setFail: '設定失敗，錯誤資訊',
+    copySuccess: '複製成功，快分享給好友吧',
+    copyFail: '目前環境不支援一鍵複製，請手動複製連結',
+  },
+};
